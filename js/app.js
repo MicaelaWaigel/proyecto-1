c778f44342182200f62211abee20975dd35969e1
MicaelaWaigel <micaawaigel@gmail.com>
2020-04-20 00:51:20 -0300

    Probando local storage

--- js/app.js
+++ js/app.js
@@ -1,18 +1,27 @@
 function cargarTabla() {
-    const tablaStorage = document.getElementById('tablaStorage');
+    let tablaStorage = document.getElementById('tablaStorage');
     const passwords = JSON.parse(window.localStorage.getItem('PASSWORDS'));
     console.log(passwords);
     if (passwords != null){
         for(i=0; i<passwords.length; i++) {
             const trOpen = '<tr>';
-            const fCell = '<td>' + parseInt(i+1, 10) + '</td>';
-            const sCell = '<td>' + passwords[i].password + '</td>';
-            const tCell = '<td>' + passwords[i].datos.puntaje + '</td>';
-            const cCell = '<td>' + passwords[i].datos.complejidad + '</td>';
+            let fCell = '<td>' + parseInt(i+1, 10) + '</td>';
+            let sCell = '<td>' + passwords[i].password + '</td>';
+            let tCell = '<td>' + passwords[i].datos.puntaje + '</td>';
+            let cCell = '<td>' + passwords[i].datos.complejidad + '</td>';
             const trClose = '</tr>';
-            const celdas = fCell + sCell + tCell + cCell;
+            let celdas = fCell + sCell + tCell + cCell;
             tablaStorage.innerHTML = trOpen + celdas + trClose;
         }
+    } else {
+        const trOpen = '<tr>';
+        const fCell = '<td>' + '1' + '</td>';
+        const sCell = '<td>-</td>';
+        const tCell = '<td>-</td>';
+        const cCell = '<td>-</td>';
+        const trClose = '</tr>';
+        const celdas = fCell + sCell + tCell + cCell;
+        tablaStorage.innerHTML = trOpen + celdas + trClose;
     }
     
 }
